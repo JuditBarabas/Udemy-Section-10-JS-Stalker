94af621a48ea580338a144de5688449b7b7c5a1f
JuditBarabas <62601033+JuditBarabas@users.noreply.github.com>
2020-10-11 18:44:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const firstName = prompt("What is your first name?");
+const lastName = prompt("What is your last name?");
+const age = prompt("How old are you?");
+
+console.log(`Your full name is ${firstName} ${lastName}.`);
+console.log(`You are ${age} years old.`);
